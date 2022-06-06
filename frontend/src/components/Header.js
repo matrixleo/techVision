ed868263da0a712ad054679dfc245461228cd431
Leon <73724759+matrixleo@users.noreply.github.com>
2022-06-07 00:19:37 +0200

    Implementing React Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import {Navbar , Container, Nav} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
   return (
     <div>
         <Navbar bg="primary" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">techVision</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                
+              <LinkContainer to="/">
+                <Navbar.Brand>techVision</Navbar.Brand>
+              </LinkContainer>
+
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />  
                 <Nav className="me-auto">
-                    <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-                    <Nav.Link href="/login"><i className="fas fa-user"></i> Login</Nav.Link>
+                <LinkContainer to="/cart">
+                    <Nav.Link ><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                  </LinkContainer>
+
+                  <LinkContainer to="/login">
+                    <Nav.Link ><i className="fas fa-user"></i> Login</Nav.Link>
+                  </LinkContainer>
                 </Nav>
                 
             </Container>
