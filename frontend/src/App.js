fa02ccb45d2b6f588962135ff12b67a5cd06c423
Eroll Uka <erollfaceit@gmail.com>
2022-06-18 19:49:21 +0200

    Created Redux Store
    Created Redux Store and installed the required thunk, redux and devdools packages used from react.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {BrowserRouter as Router,Routes ,Route} from 'react-router-dom';
 import {Container} from 'react-bootstrap';
 import Header from "./components/Header";
