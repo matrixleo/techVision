d83130992c65effa10f84f0c2c5c538541bcf452
Leon <73724759+matrixleo@users.noreply.github.com>
2022-06-19 17:38:13 +0200

    +fixing product reducers bugs

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -14,7 +14,7 @@ function HomeScreen() {
 
   useEffect(() => {
       dispatch(listProducts())
-
+      
   },[dispatch])
 
   
@@ -23,15 +23,15 @@ function HomeScreen() {
         <h1>Latest Products</h1>
 
         {loading ? <h2>Loading...</h2>
-        :error ? <h3>{error}</h3>  
-        : 
-        <Row>{products.map(product=>(
-          <Col key={product._id } sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-          </Col>
-      ))}
-      </Row>
-      }
+          :error ? <h3>{error}</h3>  
+            : 
+              <Row>{products.map(product=>(
+                <Col key={product._id } sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product} />
+                </Col>
+                ))}
+              </Row>
+        }
     </div>
   )
 }
