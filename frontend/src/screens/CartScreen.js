e29db591025ccfb31d1b736f16d747a5bbf45ecc
Eroll Uka <erollfaceit@gmail.com>
2022-06-29 22:34:07 +0200

    adding cart functionalities

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
+import {Message} from '../components/Message'
+import {addToCart} from '../actions/cartActions'
 
-function CartScreen() {
+
+
+
+
+function CartScreen({id,location,navigate}) {
+  const location = useLocation();
+  const productId = id
+  const qty = location.search
+  console.log('qty:', qty)
+
   return (
     <div>Cart</div>
   )
