5a8f7776da750a5a5edb8c0f3f61aa154469012a
Leon <73724759+matrixleo@users.noreply.github.com>
2022-10-05 16:16:42 +0200

    Add To Cart Funcionality bugs fixed + Starting project instructions

---
+++ ReadMe.txt
@@ -0,0 +1,15 @@
+===================
+Virtual Environment
+------------
+python myenv/bin/activate
+===================
+Start Back
+------------
+cd backend
+python manage.py runserver
+===================
+Start Front
+------------
+cd frontend
+npm install
+npm start
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,19 +1,29 @@
-import React, {useEffect} from 'react'
-import { Link } from 'react-router-dom'
-import {useDispatch, useSelector} from 'react-redux'
-import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
-import {Message} from '../components/Message'
+import React, { useEffect } from 'react'
+import { Link, useParams,useSearchParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
+import Message from '../components/Message'
 import {addToCart} from '../actions/cartActions'
 
+function CartScreen({match, location, history}) {
+  
+  const { id } = useParams()
+  const [searchParms] = useSearchParams();
+  const qty = Number(searchParms.get("qty"));
+  const dispatch = useDispatch()
+
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
+  console.log('cartItems:', cartItems)
 
 
+  useEffect(()=>{
+    if(id){
+      dispatch(addToCart(id, qty))
+    }
+  },[dispatch, id, qty])
 
 
-function CartScreen({id,location,navigate}) {
-  const location = useLocation();
-  const productId = id
-  const qty = location.search
-  console.log('qty:', qty)
 
   return (
     <div>Cart</div>
