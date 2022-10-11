45db6f5786978a9535568ee4a41f98e29df5d83f
Leon <73724759+matrixleo@users.noreply.github.com>
2022-10-11 15:07:32 +0200

    Jwt Customization

--- backend/base/views.py
+++ backend/base/views.py
@@ -10,6 +10,22 @@ from .serializer import ProductSerializer, serializers
 
 # Create your views here.
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+        
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 @api_view(['GET'])
 def getRoutes(request):
     routes = [
