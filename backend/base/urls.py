9e3d825600ea85728a9e053ae0aa85e7f34713a6
Leon <73724759+matrixleo@users.noreply.github.com>
2022-10-17 15:25:22 +0200

    Protected Routes

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('products/', views.getProducts, name="products"),
     path('users/profile', views.getUserProfile, name="users-profile"),
+    path('users/', views.getUsers, name="users"),
     path('products/<str:pk>/', views.getProduct, name="product"),
     path('routes/', views.getRoutes, name="routes"),
 ]
