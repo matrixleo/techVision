c44681527f45ea2a2e516bd4e5875d34263ce7b6
Leon <73724759+matrixleo@users.noreply.github.com>
2022-10-27 23:13:30 +0200

    Update User Profile

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -13,6 +13,12 @@ import {
     USER_DETAILS_SUCCESS, 
     USER_DETAILS_FAIL, 
 
+    USER_UPDATE_PROFILE_REQUEST, 
+    USER_UPDATE_PROFILE_SUCCESS, 
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_RESET,
+
+
 } from "../constants/userConstants"
 
 
@@ -62,6 +68,26 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
             return { loading: false, user: action.payload }
 
         case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+
+        case USER_UPDATE_PROFILE_RESET:
+        return {}
+        
+        default:
+            return state
+    }
+}
+
+
+export const userUpdateProfileReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+        case USER_UPDATE_PROFILE_REQUEST:
+            return {loading: true }
+
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, success:true ,userInfo: action.payload }
+
+        case USER_UPDATE_PROFILE_FAIL:
             return { loading: false, error: action.payload }
 
         default:
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -4,13 +4,16 @@ import {useDispatch, useSelector} from 'react-redux'
 import {Form, Button, Row, Col} from 'react-bootstrap'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 
 function ProfileScreen() {
 
     const [name, setName ] = useState('')
     const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
     const [message, setMessage ] = useState('')
 
     const navigate = useNavigate()
@@ -22,62 +25,101 @@ function ProfileScreen() {
     const userLogin = useSelector(state => state.userLogin)
     const{userInfo} = userLogin
 
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const{success} = userUpdateProfile
+
     useEffect(() => {
         if(!userInfo) {
             navigate('/login')
         }else{
-            if(!user || !user.name) {
+            if(!user || !user.name || success) {
+                dispatch({type:USER_UPDATE_PROFILE_RESET})
                 dispatch(getUserDetails('profile'))
             }else{
                 setName(user.name)
                 setEmail(user.email)
             }
         }
-    }, [dispatch, navigate ,userInfo, user])
+    }, [dispatch, navigate ,userInfo, user, success])
 
 
-    const submitHandler = () => {
-        console.log('abc')
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        if(password !== confirmPassword){
+            setMessage('Passwords do not match')
+        }else{
+            dispatch(updateUserProfile({
+                'id':user._id,
+                'name':name,
+                'email':email,
+                'password':password,
+            }))
+        }
     }
   return (
     <Row>
-        <Col md={3}>
-        <h2>User Profile</h2>
-        {message && <Message variant='primary'>{message}</Message>}
-        {loading && <Loader />}
-        {error && <Message variant='primary'>{error}</Message>}
-        <Form onSubmit={submitHandler}>
-            
-            <Form.Group controlId='name'>
-                <Form.Label>Name:</Form.Label>
-                <Form.Control 
-                required
-                type='name' 
-                placeholder='Enter Name'
-                value={name}
-                onChange= {(e) => setName(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
-            
-            
-            <Form.Group controlId='email'>
-                <Form.Label>Email Address:</Form.Label>
-                <Form.Control 
-                required
-                type='email' 
-                placeholder='Enter Email'
-                value={email}
-                onChange= {(e) => setEmail(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
+         <Col md={3}>
+                <h2>User Profile</h2>
 
-            
+                {message && <Message variant='danger'>{message}</Message>}
+                {error && <Message variant='danger'>{error}</Message>}
+                <Form onSubmit={submitHandler}>
 
-            <Button type='submit' variant='primary' id="signup">Update</Button>
-            </Form>
-        </Col>
+                    <Form.Group controlId='name'>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            required
+                            type='name'
+                            placeholder='Enter name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        >
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId='email'>
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control
+                            required
+                            type='email'
+                            placeholder='Enter Email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        >
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId='password'>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+
+                            type='password'
+                            placeholder='Enter Password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        >
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Form.Group controlId='passwordConfirm'>
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+
+                            type='password'
+                            placeholder='Confirm Password'
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                        >
+                        </Form.Control>
+                    </Form.Group>
+
+                    <Button type='submit' variant='primary'>
+                        Update
+                </Button>
+
+                </Form>
+            </Col>
 
         <Col md={9}>
         <h2>My Orders</h2>
