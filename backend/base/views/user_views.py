596512bb2d75ba241ef9e332308541d06bacae88
Leon <73724759+matrixleo@users.noreply.github.com>
2022-12-09 03:05:25 +0100

    Get User By ID & Update User Endpoints

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -75,11 +75,37 @@ def getUserProfile(request):
     serializers = UserSerializer(users, many = False)
     return Response(serializers.data)
 
+
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
 def getUsers(request):
     users = User.objects.all()
     serializers = UserSerializer(users, many = True)
+    return Response(serializers.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUserById(request,pk):
+    user = User.objects.get(id=pk)
+    serializers = UserSerializer(user, many = False)
+    return Response(serializers.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request,pk):
+
+    user = User.objects.get(id=pk)
+
+    data = request.data
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+    user.is_staff = data['isAdmin']
+    
+    user.save()
+
+    serializers = UserSerializerWithToken(user, many = False)
+
     return Response(serializers.data)
 
 
