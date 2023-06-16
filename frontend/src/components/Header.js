30d823ae83ada7c4f0da2e97baa2d1a552f9a3ab
Eroll Uka <74380038+Eroll-Uka@users.noreply.github.com>
2023-06-16 19:52:40 +0200

    Added Products and Orders on Admin Page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -35,6 +35,16 @@ function Header() {
                                 <LinkContainer to='/admin/userlist'>
                                     <NavDropdown.Item>Users</NavDropdown.Item>
                                 </LinkContainer>
+
+                                <LinkContainer to='/admin/productlist'>
+                                    <NavDropdown.Item>Products</NavDropdown.Item>
+                                </LinkContainer>
+
+                                <LinkContainer to='/admin/orderlist'>
+                                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                                </LinkContainer>
+
+
                             </NavDropdown>
                             )}
 
